ab3af116e0520e4742a7ae3d186b574051ede06b
pzproger <pzproger@gmail.com>
2023-02-13 01:06:27 +0300

    add autoload file

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-Hello, Hexlet!
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Файл не включается напрямую
+// Он загрузится автоматически благодаря автозагрузке
+use Hexlet\Php\Runner;
+
+print_r(Runner\run());
